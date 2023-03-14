e67668160e63293801a0881fa31112287dcbed0b
iwata5342 <126980328+iwata5342@users.noreply.github.com>
2023-03-14 16:21:02 +0900

    Delete server directory

--- server/getcommands.js
+++
@@ -1,8 +0,0 @@
-this.pool = require("./db");
-
-pool.query(
-    "SELECT ;", 
-    [attr], [dname], (error, results) => {
-    if (error) throw error;
-    return res.status(200).json(results.rows);
-});
